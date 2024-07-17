b08c8454de30f5d28c68c6f5e32bab13182c0d3c
RaJansuMan <rajansuman180@gmail.com>
2024-07-18 00:37:28 +0530

    Locate Screen ui

--- app/src/main/java/com/selfproject/cordsapp/presentation/locate/LocateScreen.kt
+++ app/src/main/java/com/selfproject/cordsapp/presentation/locate/LocateScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -22,6 +23,8 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -39,9 +42,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.selfproject.cordsapp.R
 import com.selfproject.cordsapp.ui.theme.CordsAppTheme
 
@@ -51,6 +60,7 @@ import com.selfproject.cordsapp.ui.theme.CordsAppTheme
 )
 @Composable
 fun LocateScreen() {
+    val localDensity = LocalDensity.current
     var sheetState by remember { mutableStateOf(BottomSheetState.COLLAPSED) }
     val onSheetStateChanged: (BottomSheetState) -> Unit = { newState ->
         sheetState = newState
@@ -58,38 +68,53 @@ fun LocateScreen() {
     val topHeight = 60.dp
     val halfExpandedHeight = 135.dp
 
+    var bottomCardHeight by remember { mutableStateOf(0.dp) }
+    var isBottomCardMeasured by remember { mutableStateOf(false) }
+
+
+
     Box(modifier = Modifier.fillMaxSize()) {
 //        MapBoxMap(point = null)
-      ExpandedSearchView(
+        ExpandedSearchView(
+            modifier = Modifier
+                .height(48.dp)
+                .padding(end = 8.dp, top = 8.dp)
+                .align(Alignment.TopEnd)
+                .alpha(0.9F),
+            searchDisplay = "",
+            onSearchDisplayChanged = {},
+            onSearchDisplayClosed = {}
+        )
+        if (!isBottomCardMeasured) {
+            BottomCard(
                 modifier = Modifier
-                    .height(48.dp)
-                    .padding(end = 8.dp, top = 8.dp)
-                    .align(Alignment.TopEnd)
-                    .alpha(0.9F),
-                searchDisplay = "",
-                onSearchDisplayChanged = {},
-                onSearchDisplayClosed = {}
-            )
-        CustomBottomSheet(
-            modifier = Modifier.align(Alignment.BottomCenter),
-            sheetState = sheetState,
-            fullyExpandedHeight = 400.dp,
-            halfExpandedHeight = halfExpandedHeight,
-            minHeight = topHeight,
-            onSheetStateChanged = onSheetStateChanged
-        ) {
-            Column {
-                TopSheet(modifier = Modifier.height(topHeight))
-                TopHeaderCard(modifier = Modifier.height(halfExpandedHeight-topHeight))
-            }
+                    .onGloballyPositioned { coordinates ->
+                        bottomCardHeight =
+                            with(localDensity) { coordinates.size.height.toDp() }
+                        isBottomCardMeasured = true
+                    }
+                    .alpha(0.0F)
 
+            )
+        }
+        if (isBottomCardMeasured) {
+            CustomBottomSheet(
+                modifier = Modifier.align(Alignment.BottomCenter),
+                sheetState = sheetState,
+                fullyExpandedHeight = bottomCardHeight + halfExpandedHeight,
+                halfExpandedHeight = halfExpandedHeight,
+                minHeight = topHeight,
+                onSheetStateChanged = onSheetStateChanged
+            ) {
+                Column {
+                    TopSheet(modifier = Modifier.height(topHeight))
+                    TopHeaderCard(modifier = Modifier.height(halfExpandedHeight - topHeight))
+                    BottomCard()
+                }
+            }
         }
     }
-}
-
-
-
-
+    }
 
 
 @Composable
@@ -101,7 +126,11 @@ fun TopSheet(modifier: Modifier = Modifier) {
             .background(color = Color.Transparent),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Box(modifier = Modifier.padding(bottom = 2.dp).align(Alignment.Bottom)) {
+        Box(
+            modifier = Modifier
+                .padding(bottom = 2.dp)
+                .align(Alignment.Bottom)
+        ) {
             IconButton(modifier = Modifier
                 .background(
                     shape = RoundedCornerShape(corner = CornerSize(10.dp)),
@@ -109,13 +138,15 @@ fun TopSheet(modifier: Modifier = Modifier) {
                 )
                 .align(Alignment.BottomCenter)
                 .padding(0.dp)
-                .size(35.dp)
-             ,
+                .size(35.dp),
                 onClick = { /*TODO*/ }) {
                 Icon(
                     painter = painterResource(id = R.drawable.baseline_gps_fixed_24),
-                    contentDescription = "Locate", tint = MaterialTheme.colorScheme.onSurfaceVariant ,
-                    modifier = Modifier.padding(0.dp).size(18.dp)
+                    contentDescription = "Locate",
+                    tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                    modifier = Modifier
+                        .padding(0.dp)
+                        .size(18.dp)
                 )
             }
         }
@@ -132,19 +163,23 @@ fun TopSheet(modifier: Modifier = Modifier) {
                 contentDescription = "Add",
                 tint = MaterialTheme.colorScheme.onSurfaceVariant,
 
-            )
+                )
         }
     }
 }
 
 @Composable
-fun TopHeaderCard(modifier: Modifier=Modifier) {
+fun TopHeaderCard(modifier: Modifier = Modifier) {
     Card(
         modifier = modifier
-            .fillMaxWidth()
-            ,
+            .fillMaxWidth(),
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
-        shape = RoundedCornerShape(topEnd = 8.dp, topStart = 8.dp, bottomEnd = 0.dp, bottomStart = 0.dp),
+        shape = RoundedCornerShape(
+            topEnd = 8.dp,
+            topStart = 8.dp,
+            bottomEnd = 0.dp,
+            bottomStart = 0.dp
+        ),
         colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
     ) {
         Box(modifier = Modifier.fillMaxSize()) {
@@ -184,26 +219,109 @@ fun TopHeaderCard(modifier: Modifier=Modifier) {
                 )
             }
 
-            Text(text = "Point no : 0", modifier = Modifier.align(Alignment.Center), color = MaterialTheme.colorScheme.onSurfaceVariant)
+            Text(
+                text = "Point no : 0",
+                modifier = Modifier.align(Alignment.Center),
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp
+            )
         }
     }
-    
+
 }
- @Composable
- fun BottomCard(modifier: Modifier=Modifier) {
-     Card(
-         modifier = modifier
-             .fillMaxWidth(),
-         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
-         shape = RoundedCornerShape(
-             topEnd = 8.dp,
-             topStart = 8.dp,
-             bottomEnd = 0.dp,
-             bottomStart = 0.dp
-         ),
-         colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
-     ) {}
- }
+@Composable
+fun BottomCard(modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        shape = RoundedCornerShape(
+            0.dp
+        ),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
+    ) {
+        Column {
+            SectionHeader(modifier = Modifier.padding(top = 5.dp), value = "Description")
+            Text(
+                modifier = Modifier.padding(start = 20.dp, top = 8.dp),
+                text = "NEW Point",
+                textAlign = TextAlign.Justify, color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+            SectionHeader(modifier = Modifier.padding(top = 5.dp), value = "WGS Coordinates")
+            TextRow(modifier = Modifier.fillMaxWidth(), field = "Latitude   :", value = "1.4588537")
+            TextRow(modifier = Modifier.fillMaxWidth(), field = "Longitude :", value = "1.4588537")
+//            SectionHeader(modifier= Modifier.padding(top = 5.dp),value = "UTM Coordinates")
+//            TextRow(modifier=Modifier.fillMaxWidth(), field = "Easting    :", value = "1.4588537")
+//            TextRow(modifier=Modifier.fillMaxWidth(), field = "Northing   :", value = "1.4588537")
+//            TextRow(modifier=Modifier.fillMaxWidth(), field = "UTM Zone   :", value = "1.4588537")
+//            TextRow(modifier=Modifier.fillMaxWidth(), field = "UTM Letter   :", value = "1.4588537")
+            SectionHeader(modifier = Modifier.padding(top = 5.dp), value = "Elevation")
+            TextRow(
+                modifier = Modifier.fillMaxWidth(),
+                field = "Ellipsoidal   :",
+                value = "1.4588537"
+            )
+//            TextRow(modifier=Modifier.fillMaxWidth(), field = "   :", value = "1.4588537")
+//            TextRow(modifier=Modifier.fillMaxWidth(), field = "UTM Letter   :", value = "1.4588537")
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp), horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                Button(
+                    onClick = { },
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
+                ) {
+                    Text(text = "DELETE POINT", style = TextStyle(fontWeight = FontWeight.Bold))
+                }
+                Button(
+                    onClick = { },
+                    shape = RoundedCornerShape(8.dp) // Rounded corners
+                ) {
+                    Text(text = "POINT DETAILS", style = TextStyle(fontWeight = FontWeight.Bold))
+                }
+            }
+
+        }
+    }
+}
+
+
+@Composable
+private fun SectionHeader(modifier: Modifier = Modifier, value: String, spacer: Boolean = true) {
+    Column(modifier = modifier) {
+        if (spacer) {
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(MaterialTheme.colorScheme.onSurfaceVariant)
+            )
+        }
+        Text(
+            modifier = Modifier.padding(start = 8.dp, top = 5.dp, bottom = 2.dp),
+            text = value,
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary,
+                fontSize = 18.sp
+            )
+        )
+    }
+}
+
+@Composable
+fun TextRow(modifier: Modifier = Modifier, field: String, value: String) {
+    Row(
+        modifier = modifier.padding(start = 20.dp, top = 8.dp),
+        horizontalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+        Text(text = "$field ", color = MaterialTheme.colorScheme.onSurfaceVariant)
+        Text(text = value, color = MaterialTheme.colorScheme.onSurfaceVariant)
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
